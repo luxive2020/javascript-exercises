8ed2bf7746a924f89457946e3814bd27e855e264
Tajudeen Taiwo <tajudeentaiwo13@gmail.com>
2025-01-04 16:57:48 +0100

    Done with exercise-02 repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(stringValue, num) {
+    let result = '';
+    if (Math.sign(num) === -1){
+        result+= 'ERROR'
+    }
+    
+    for( let i = 0; i<num; i++){
+        result+= stringValue;
+    }
+    return result;
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
