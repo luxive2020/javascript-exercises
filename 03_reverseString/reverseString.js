4843523af46d686dd820d312add693a2b0250630
Tajudeen Taiwo <tajudeentaiwo13@gmail.com>
2025-01-06 08:36:43 +0100

    Exercise-4(String Reversing)  done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let myString = string;
+let mySplitString = myString.split('');
+let myReverseArray = mySplitString.reverse();
+let myReverseString = myReverseArray.join('')
+return myReverseString;
 };
 
 // Do not edit below this line
