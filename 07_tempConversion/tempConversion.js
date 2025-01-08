4472c97c52f7fa941c8584a562ea674b5a79d436
Tajudeen Taiwo <tajudeentaiwo13@gmail.com>
2025-01-08 03:42:11 +0100

    exercise-7 celsius-fahrenheit temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahVal) {
+  let fahValConvertToNumber = Number(fahVal);
+  const fractionalPart = 5/9;
+  let convertToCel = (fahValConvertToNumber-32)*fractionalPart;
+ let finalResult = Number(convertToCel.toFixed(1))
+  return finalResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celVal) {
+  let celValConvertToNumber = Number(celVal);
+  const fractionalPart = 9/5;
+  let convertToFah = (celValConvertToNumber*fractionalPart)+32;
+ let finalResult = Number(convertToFah.toFixed(1))
+  return finalResult;
 };
 
 // Do not edit below this line
